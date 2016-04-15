cb458aeebe412407eec9b11a26bb58258a187e1e
Karina <karina@alisons-MacBook-Air.local>
2016-04-15 13:40:23 -0500

    final commit.

--- do_while.php
+++ do_while.php
@@ -14,3 +14,11 @@ do {
     echo "{$number}" . PHP_EOL;
     $number -= 5;
 } while ($number >= -10);
+
+//Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a is less than 1,000,000. Output should equal:
+$a = 2;
+
+do {
+    echo "{$a}" . PHP_EOL;
+    $a *= $a;
+} while ($a <= 1000000);
