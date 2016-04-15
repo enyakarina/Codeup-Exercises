e4e8f445a47bce851a763bdeca87c16a8e1b9799
Karina <karina@alisons-MacBook-Air.local>
2016-04-15 13:35:29 -0500

    2nd commit. Function that takes 100 and subtracts 5 until it reaches -10.

--- do_while.php
+++ do_while.php
@@ -6,3 +6,11 @@ do {
     echo "{$num}" . PHP_EOL;
     $num += 2;
 } while ($num <= 100);
+
+//Alter your loop to count backwards by 5's from 100 to -10.
+$number = 100;
+
+do {
+    echo "{$number}" . PHP_EOL;
+    $number -= 5;
+} while ($number >= -10);
