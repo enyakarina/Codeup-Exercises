9ea5e0e1cc75dcd315fc1cc344311a9262d02fa4
Karina <karina@alisons-MacBook-Air.local>
2016-04-15 10:14:33 -0500

    Initial commit. It's just an exercise on arrays.

---
+++ array_exercise.php
@@ -0,0 +1,43 @@
+<?php
+
+$numbers = array(1, 2, 3, 4, 5);
+
+//var_dump($numbers);
+
+print_r($numbers);
+
+$numeros = [
+    1,
+    2,
+    3,
+    4,
+    5
+];
+
+//var_dump($numeros);
+print_r($numeros);
+
+echo $numeros[3] . PHP_EOL;
+
+$test = [
+    'person1' => [
+    'first_name' => 'Kari',
+    'last_name' => 'Palomino',
+    'email' => 'me@email.com',
+    'phone' => '1111111111'
+    ],
+
+    'person2' => [
+    'first_name' =>'Donkey',
+    'last_name' => 'Kong',
+    'email' => 'banana@email.com',
+    'phone' => '2222222222'
+    ],
+
+    'person3' => [
+    'first_name' => 'Comic',
+    'last_name' => 'Sans',
+    'email' => 'worst@font.com',
+    'phone' => '3333333333'
+    ]
+];
